966be7c932729dd080ee3a4de29b2e0b3799d057
leondutoit <dutoit.leon@gmail.com>
2018-08-27 15:51:35 +0200

    start implementing sql based testing

--- testing.sql
+++ testing.sql
@@ -1,37 +1,53 @@
 
-----------------
--- Use the model
-----------------
+-- `/rpc/table_create`
+select table_create('{"table_name": "people", "columns": [ {"name": "name", "type": "text"}, {"name": "age", "type": "int"} ]}'::json, 'mac');
+\d+ people
 
--- need to create the roles before inserting data
--- add a register request /rpc/user_register -> hash(id, salt), create role
-create role role1;
-grant role1 to authenticator;
-grant select on group_memberships to role1;
+-- `/rpc/user_create`
+select user_create('gustav');
+select user_create('hannah');
+\du
 
-create role role2;
-grant role2 to authenticator;
-grant select on group_memberships to role2;
+-- `/rpc/group_create`
+select group_create('project_members');
+\du
 
-create role role3;
-grant role3 to authenticator;
-grant select on group_memberships to role2;
-
-set role role1;
-insert into t1 (x) values (1);
+set role gustav;
+insert into people (name, age) values ('Gustav de la Croix', 1);
+-- add test to ensure that row_owner cannot be selected
+select name, age from people;
 set role authenticator;
 
-set role role2;
-insert into t1 (x) values (2);
+set role gustav;
+insert into people (name, age) values ('Hannah le Roux', 29);
+-- add test to ensure that row_owner cannot be selected
+select name, age from people;
 set role authenticator;
 
-set role role3;
-insert into t1 (x) values (3);
+-- `/rpc/group_add_members`
+-- limit to admin_user
+select group_add_members('{"memberships": [{"user":"gustav", "group":"project_members"}, {"user":"hannah", "group":"project_members"}]}'::json);
+\du
+
+set role gustav;
+select name, age from people;
 set role authenticator;
 
--- only owners can operate on their own tables
+set role gustav;
+select name, age from people;
 set role authenticator;
-table t1;
 
-set role role1;
-table t1;
+-- `/rpc/group_list_members`
+
+
+-- `/rpc/group_remove_members`
+
+
+set role gustav;
+
+-- `/rpc/user_delete_data`
+
+
+-- `/rpc/user_delete`
+
+-- `/rpc/group_delete`
