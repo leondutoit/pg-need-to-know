a056fd63205090d167c7b0d5fb2d85ed970ee42e
leondutoit <dutoit.leon@gmail.com>
2018-08-27 11:47:46 +0200

    begin writing functions - to be exposed in rpc interface

--- poc.sql
+++ poc.sql
@@ -7,6 +7,10 @@ create role authenticator;
 grant authenticator to tsd_backend_utv_user;
 \du
 
+------------
+-- functions
+------------
+
 create or replace view group_memberships as
 select _group, _role from
     (select * from
@@ -35,47 +39,147 @@ create or replace function roles_have_common_group(_current_role text, _current_
     return _res;
     end;
 $$ language plpgsql;
-\df
 
--- a table where row owners can see their own data
--- and delete or update their own data
--- and non-owners can see the data when they are in
--- the same group as the row_owner
-drop table if exists t1;
-create table t1(row_owner text default current_user, x int);
-alter table t1 owner to authenticator;
-alter table t1 enable row level security;
-grant insert, select, update, delete on t1 to public; -- constrained by RLS
+drop function if exists table_create(json, text, int);
+create or replace function table_create(definition json, type text, form_id int default 0)
+    returns text as $$
+    declare _res text;
+    begin
+        if type = 'mac' then
+            select parse_mac_table_def(definition) into _res;
+            return _res;
+        elsif type = 'generic' then
+            select parse_generic_table_def(definition) into _res;
+            return _res;
+        else
+            raise exception using message = 'Unrecognised table definition type.';
+        end if;
+    end;
+$$ language plpgsql;
 
--- policies (https://www.postgresql.org/docs/9.6/static/sql-createpolicy.html)
--- USING: existing rows (select, delete)
--- CHECK: new rows (insert, update)
-create policy row_ownership_insert_policy on t1 for insert with check (true);
-create policy row_ownership_select_policy on t1 for select using (row_owner = current_user);
-create policy row_ownership_delete_policy on t1 for delete using (row_owner = current_user);
-create policy row_ownership_select_group_policy on t1 for select using (roles_have_common_group(current_user::text, row_owner));
-create policy row_owbership_update_policy on t1 for update using (row_owner = current_user) with check (row_owner = current_user);
+drop function if exists parse_mac_table_def(json);
+create or replace function parse_mac_table_def(definition json)
+    returns text as $$
+    declare _table_name text;
+    declare _columns json;
+    declare _colname text;
+    declare _dtype text;
+    declare _i json;
+    declare _pk boolean;
+    declare _nn boolean;
+    begin
+        _columns := definition->'columns';
+        _table_name := definition->>'table_name';
+        execute 'create table if not exists ' || _table_name || '()';
+        for _i in select * from json_array_elements(_columns) loop
+            select sql_type_from_generic_type(_i->>'type') into _dtype;
+            select _i->>'name' into _colname;
+            begin
+                -- can use if not exists when postgres 9.6 is running in tsd
+                execute 'alter table ' || _table_name || ' add column ' || _colname || ' ' || _dtype;
+            exception
+                when duplicate_column then raise notice 'column % already exists', _colname;
+            end;
+            begin
+                select _i->'constraints'->'primary_key' into _pk;
+                if _pk is not null then
+                    begin
+                        execute 'alter table ' || _table_name || ' add primary key ' || '(' || _colname || ')';
+                    exception
+                        when invalid_table_definition then raise notice 'primary key already exists';
+                    end;
+                end if;
+            end;
+            begin
+                select _i->'constraints'->'not_null' into _nn;
+                if _nn is not null then
+                    execute 'alter table ' || _table_name || ' alter column ' || _colname || ' set not null';
+                end if;
+            end;
+        end loop;
+        execute 'alter table ' || _table_name || ' owner to authenticator';
+        execute 'alter table ' || _table_name || ' enable row level security';
+        execute 'grant insert, select, update, delete on ' || _table_name || ' to public';
+        execute 'grant execute on function roles_have_common_group(text, text) to public';
+        execute 'create policy row_ownership_insert_policy on ' || _table_name || ' for insert with check (true)';
+        execute 'create policy row_ownership_select_policy on ' || _table_name || ' for select using (row_owner = current_user)';
+        execute 'create policy row_ownership_delete_policy on ' || _table_name || ' for delete using (row_owner = current_user)';
+        execute 'create policy row_ownership_select_group_policy on ' || _table_name || ' for select using (roles_have_common_group(current_user::text, row_owner))';
+        execute 'create policy row_owbership_update_policy on ' || _table_name || ' for update using (row_owner = current_user) with check (row_owner = current_user)';
+        return 'Success';
+    end;
+$$ language plpgsql;
 
-\d+ t1
+
+drop function if exists parse_generic_table_def(json);
+create or replace function parse_generic_table_def(definition json)
+    returns text as $$
+    declare _table_name text;
+    declare _columns json;
+    declare _colname text;
+    declare _dtype text;
+    declare _i json;
+    declare _pk boolean;
+    declare _nn boolean;
+    begin
+        _columns := definition->'columns';
+        _table_name := definition->>'table_name';
+        execute 'create table if not exists ' || _table_name || '()';
+        for _i in select * from json_array_elements(_columns) loop
+            select sql_type_from_generic_type(_i->>'type') into _dtype;
+            select _i->>'name' into _colname;
+            begin
+                -- can use if not exists when postgres 9.6 is running in tsd
+                execute 'alter table ' || _table_name || ' add column ' || _colname || ' ' || _dtype;
+            exception
+                when duplicate_column then raise notice 'column % already exists', _colname;
+            end;
+            begin
+                select _i->'constraints'->'primary_key' into _pk;
+                if _pk is not null then
+                    begin
+                        execute 'alter table ' || _table_name || ' add primary key ' || '(' || _colname || ')';
+                    exception
+                        when invalid_table_definition then raise notice 'primary key already exists';
+                    end;
+                end if;
+            end;
+            begin
+                select _i->'constraints'->'not_null' into _nn;
+                if _nn is not null then
+                    execute 'alter table ' || _table_name || ' alter column ' || _colname || ' set not null';
+                end if;
+            end;
+        end loop;
+        execute 'alter table ' || _table_name || ' owner to import_user';
+        return 'Success';
+    end;
+$$ language plpgsql;
+
+-- func: create role
+-- func: create group
+-- func: add member
+-- func: remove member
+-- func: delete all my data, put in audit table
+
+----------------
+-- Use the model
+----------------
 
 -- need to create the roles before inserting data
 -- add a register request /rpc/user_register -> hash(id, salt), create role
 create role role1;
 grant role1 to authenticator;
 grant select on group_memberships to role1;
-grant execute on function roles_have_common_group(text, text) to role1;
 
 create role role2;
 grant role2 to authenticator;
 grant select on group_memberships to role2;
-grant execute on function roles_have_common_group(text, text) to role2;
 
 create role role3;
 grant role3 to authenticator;
 grant select on group_memberships to role2;
-grant execute on function roles_have_common_group(text, text) to role2;
 
--- simulate API-based inserts
 set role role1;
 insert into t1 (x) values (1);
 set role authenticator;
