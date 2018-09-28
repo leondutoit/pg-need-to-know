47a9785622d77234b4f860357d8ffb4b35b6fdff
leondutoit <dutoit.leon@gmail.com>
2018-09-28 12:01:04 +0200

    re-enable test_user_list

--- src/testing.sql
+++ src/testing.sql
@@ -695,7 +695,7 @@ create or replace function run_tests()
         assert (select test_group_list()), 'ERROR: test_group_list';
         assert (select test_group_list_members()), 'ERROR: test_group_list_members';
         assert (select test_user_groups()), 'ERROR: test_user_groups';
-        --assert (select test_user_list()), 'ERROR: test_user_list';
+        assert (select test_user_list()), 'ERROR: test_user_list';
 
         --assert (select test_table_group_access_management()), 'ERROR: test_table_group_access_management';
         --assert (select test_group_membership_data_access_policies()), 'ERROR: test_group_membership_data_access_policies';
