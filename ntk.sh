b51d55196b3bc79eed37a9626d55e2e6199520a5
leondutoit <dutoit.leon@gmail.com>
2018-10-11 13:09:52 +0200

    add hostname to ntk.sh

--- ntk.sh
+++ ntk.sh
@@ -16,6 +16,7 @@ _guide="\
     export SUPERUSER=db-super-user-name
     export DBOWNER=db-owner-user-name
     export DBNAME=db-name
+    export DBHOST=db-host-name
     export JWT_SECRET=mysecret
 
     Create a .pgpass file so you can connect to the DB.
@@ -31,10 +32,10 @@ _guide="\
 "
 
 setup() {
-    psql -U $SUPERUSER -d $DBNAME -f ./src/need-to-know.sql
-    psql -U $SUPERUSER -d $DBNAME -f ./src/groups.sql
-    psql -U $SUPERUSER -d $DBNAME -f ./src/need-to-know-token.sql
-    psql -U $SUPERUSER -d $DBNAME -c "insert into jwt.secret_store values ('$JWT_SECRET');"
+    psql -h $DBHOST -U $SUPERUSER -d $DBNAME -f ./src/need-to-know.sql
+    psql -h $DBHOST -U $SUPERUSER -d $DBNAME -f ./src/groups.sql
+    psql -h $DBHOST -U $SUPERUSER -d $DBNAME -f ./src/need-to-know-token.sql
+    psql -h $DBHOST -U $SUPERUSER -d $DBNAME -c "insert into jwt.secret_store values ('$JWT_SECRET');"
     echo 'pg-need-to-know setup complete, make sure to clear the JWT_SECRET environment variable :)'
 }
 
